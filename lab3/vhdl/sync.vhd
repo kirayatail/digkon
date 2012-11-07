424c62b8e2455575a41c6845b77570d5438aea49
unknown <erikax@CE-PC32.net.chalmers.se>
2012-11-07 13:40:20 +0100

    Trying to find source of a bug

--- lab3/vhdl/sync.vhd
+++ lab3/vhdl/sync.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity sync is
     Port ( clk : in  STD_LOGIC;
            reset : in  STD_LOGIC;
-           levin : in  STD_LOGIC;
+           input : in  STD_LOGIC;
            sync : out  STD_LOGIC);
 end sync;
 architecture Behavioral of sync is
@@ -33,7 +33,7 @@ begin
 process (clk)
       begin
       if clk'event and clk = '1' then
-q <= levin;
+				q <= input;
             sync <= q;
       end if;
       end process;
--- lab3/vhdl/vhdltop.vhd
+++ lab3/vhdl/vhdltop.vhd
@@ -22,7 +22,7 @@ architecture Behavioral of vhdltop is
 component sync is
     Port ( clk : in  STD_LOGIC;
            reset : in  STD_LOGIC;
-           levin : in  STD_LOGIC;
+           input : in  STD_LOGIC;
            sync : out  STD_LOGIC);
 end component;
 component lev2puls is
@@ -47,9 +47,10 @@ signal levin,puls,reset: std_logic;
 signal dut: std_LOGIC_VECTOR(7 downto 0);
 begin
 reset <= not resin;
-x1: sync port map (clk,reset, dav, levin);
-x2: lev2puls port map(clk,reset,levin,puls);
 x3: decoder38e port map(din,puls,dut);
+
+x1: sync port map (clk,reset,dav,levin);
+x2: lev2puls port map(clk,reset,levin,puls);
 x4: puls2lev port map(clk,reset,dut(0),dut(1),borrmotor);
 x5: puls2lev port map(clk,reset,dut(2),dut(3),solenoid);
 end Behavioral;
